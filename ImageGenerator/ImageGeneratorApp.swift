80c23f681f47fec143cb9238bb42100ae16f641f
Taras <45391115+prystupataras@users.noreply.github.com>
2024-09-02 11:48:23 +0300

    Initial Commit

---
+++ ImageGenerator/ImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGeneratorApp.swift
+//  ImageGenerator
+//
+//  Created by Prystupa Taras on 02.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct ImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
